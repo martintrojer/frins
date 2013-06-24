de4bbca5961df37e365c6b0bfaf24b01252e6794
Martin Trojer <martin.trojer@gmail.com>
2013-06-24 13:53:31 +0100

    more calctests

--- src/test/scala/frins/SampleCalcTest.scala
+++ src/test/scala/frins/SampleCalcTest.scala
@@ -18,17 +18,17 @@ class SampleCalcTest extends FunSuite {
   test("water") {
     assert(checkNumber(
       Number(552960.0/77),
-      N(10, 'feet) * N(12, 'feet) * N(8, 'feet) to ('gallons)
+      N(10, 'feet) * N(12, 'feet) * N(8, 'feet) to 'gallons
     ))
 
     assert(checkNumber(
       Number(2718417272832.0/45359237),
-      N(10, 'feet) * N(12, 'feet) * N(8, 'feet) * 'water to ('pounds)
+      N(10, 'feet) * N(12, 'feet) * N(8, 'feet) * 'water to 'pounds
     ))
 
     assert(checkNumber(
       Number(5669904625.0/10618817472.0),
-      N(2, 'tons) / (N(10 * 12) * 'feet * 'feet * 'water) to ('feet)
+      N(2, 'tons) / (N(10 * 12) * 'feet * 'feet * 'water) to 'feet
     ))
   }
 
@@ -37,11 +37,43 @@ class SampleCalcTest extends FunSuite {
     N(86481 - 41601, 'thousand, 'dollars) / ('$2001_06_30 - '$2000_12_31))
 
   test("dates") {
+    assert(checkNumber(
+      Number(60224381.0/359040),
+      N(41601, 'thousand, 'dollars) / 'burnrate to 'days
+    ))
+
     expectResult("Fri Dec 14 16:41:38 GMT 2001") {
       (N('$2001_06_30) + (N(41601, 'thousand, 'dollars) / 'burnrate) toDate).toString
 
     }
   }
 
+  test("barrels") {
+    assert(checkNumber(
+      N(368175625.0/129048129),
+      N('half, 'ton) to('barrels, 'water)
+    ))
 
+    assert(checkNumber(
+      N(46037384521821.0/19375000000000.0),
+      N(2, 'fathoms, 'water) * 'gravity * 'barrel to N(40, 'watts, 'minutes)
+    ))
+  }
+
+  test("Calories") {
+    assert(checkNumber(
+      N(15345794840607.0/11266562500000.0),
+      N(2, 'fathoms, 'water) * 'gravity * 'barrel to 'Calories
+    ))
+
+    assert(checkNumber(
+      N(1163.0/12),
+      N(2000, 'Calories, '_day) to 'watts
+    ))
+
+    assert(checkNumber(
+      N(800000000000.0/43161375789.0),
+      N(1100, 'W) * 30 * 'sec / (N(27, 'oz) * 'calorie / 'gram * '_degC) to 'degF
+    ))
+  }
 }
